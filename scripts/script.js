383433bbfbba7e26a4008454bd6bdf761e84ed67
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-09 16:55:44 -0400

    Add functions for calculation

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const add = (a, c) => {
+    return Number(a) + Number(c);
+};
+
+const subtract = (a, c) => {
+    return a - c;
+};
+
+const multiply = (a, c) => {
+    return a * c;
+};
+
+const divide = (a, c) => {
+    return a / c;
+};
